646d72410d6d8c44cf1bebca9befbdcae85c2757
sabbir-anwar <protik26@gmail.com>
2020-11-02 16:48:52 +0600

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, {useState} from "react";
 import { Switch,Grid, Typography, Button, Paper } from "@material-ui/core";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-
+// import "./index.css";
+// import Header from "./Header";
+// import Content from "./Content";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -12,6 +14,18 @@ function App() {
     },
   });
   return (
+    // <Grid container direction="column">
+    //   <Grid item>
+    //     <Header />
+    //   </Grid>
+    //   <Grid item container>
+    //     <Grid item xs={0} sm={2} />
+    //     <Grid item xs={12} sm={8}>
+    //       <Content />
+    //     <Grid item xs={0} sm={2} />
+    //     </Grid>
+    //   </Grid>
+    // </Grid>
     <ThemeProvider theme={theme}>
       <Paper style={{height: "100vh"}}>
         <Grid container direction="column">
